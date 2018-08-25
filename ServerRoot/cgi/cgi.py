5d732b351d206136a1c2c055b6e845bdf7e46416
dlxu1 <dl_xu@foxmail.com>
2018-08-25 21:02:41 +0800

    EpollHttpServer

--- ServerRoot/cgi/cgi.py
+++ ServerRoot/cgi/cgi.py
@@ -6,12 +6,16 @@ def deal_with():
 	str="0"
 	if cmp(os.getenv("math"),"GET")==0:
 	    str=os.getenv("query_string")
-	#print str
-	sub1,sub2= str.split("&")
-	sub1=sub1[2:]
-	sub2=sub2[2:]
+	ret=1;
+	#sub1,sub2= str.split("&")
+	sub1=str[2:]
+	sub1=int(sub1)
+	for i in range(1,sub1):
+	    ret*=i
+	#sub2=sub2[2:]
 	print "计算结果：" 
-	print sub1+'*'+sub2+"=%d"%(int(sub2)*int(sub1))
+	#print sub1+'*'+sub2+"=%d"%(int(sub2)*int(sub1))
+	print ret
 
 if __name__ == "__main__":
 	deal_with()
